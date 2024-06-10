c822d7106a6378cccd176f109e069f923b78e802
LeticiaMissena <163003394+LeticiaMissena@users.noreply.github.com>
2024-06-10 10:59:57 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,3 +5,6 @@ class Livros(models.Model):
     titulo = models.CharField(max_length=150)
     autor = models.CharField(max_length=150)  
     identificacao = models.IntegerField()
+    numPaginas = models.IntegerField()
+    ano = models.IntegerField()
+    sinopse = models.Textfield(max_Length=10000)
